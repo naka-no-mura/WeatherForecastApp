30b130b3de5d49a6083ea96dc2620a0c481b3a0c
naka-no-mura <nakanomura.ryu@gmail.com>
2022-11-23 06:26:40 +0900

    Initial Commit

---
+++ WeatherForecastApp/WeatherForecastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastAppApp.swift
+//  WeatherForecastApp
+//
+//  Created by r.nakamura on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
